aa79a33d5afede8daaa606b2ae78cde4e54aaa99
Kuldeep Pisda <pisdak79@gmail.com>
2023-09-16 20:46:19 +0530

    UPdated lambda

--- lambdas/presigned-url-function/lambda.py
+++ lambdas/presigned-url-function/lambda.py
@@ -1,78 +1,71 @@
-import json
 import os
+import json
 import boto3
 
+# Initialize the S3 client
+s3_client = boto3.client('s3')
 
-def check_and_create_path(s3_client, bucket_name, path):
-    """
-    This function checks if the path exists in the bucket and creates it if it doesn't.
-    :param s3_client:
-    :param bucket_name:
-    :param path:
-    :return:
-    """
+# Read the EXPIRES_IN value from environment variables; default to 3600 seconds (1 hour) if not set
+EXPIRES_IN = int(os.environ.get('EXPIRES_IN', 3600))
+
+
+# Function to generate a presigned URL for uploading a file to S3
+def generate_presigned_upload_url(bucket, folder, file_name):
     try:
-        s3_client.head_object(Bucket=bucket_name, Key=path)
+        key = f"{folder}/{file_name}"
+        presigned_url = s3_client.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': bucket,
+                'Key': key
+            },
+            ExpiresIn=EXPIRES_IN
+        )
+        return presigned_url, None
     except Exception as e:
-        if e.response['Error']['Code'] == '404':
-            s3_client.put_object(Bucket=bucket_name, Key=path)
-        else:
-            raise
+        return None, str(e)
 
 
+# The main Lambda function handler
 def lambda_handler(event, context):
-    """
-    This function generates pre-signed URLs for the files that are going to be uploaded to S3.
-    The reqeust validations are done on the APIGateway level so we don't need to do them here.
-    Also, in case of error we just append None to the list of pre-signed URLs for that file.
-    :param event:
-    :param context:
-    :return dict:
-    """
+    try:
+        # Parse the 'body' from the event payload and load it as JSON
+        body = json.loads(event['body'])
 
-    s3 = boto3.client('s3')
+        # Extract the 'files' list from the parsed JSON body
+        files = body.get('files', [])
 
-    body = json.loads(event['body'])
+        response = []
 
-    expires_in = int(os.getenv('EXPIRES_IN', 3600))
+        for file in files:
+            bucket = file['bucket']
+            folder = file['folder']
+            file_name = file['file_name']
 
-    files = body.get('files', [])
-    presigned_urls = []
+            presigned_url, error = generate_presigned_upload_url(bucket, folder, file_name)
 
-    for file in files:
-        # Since we already have the schema in place we don't need to do the request validations here anymore
-        bucket_name = file.get('bucket')
-        file_name = file.get('file_name')
-        folder = file.get('folder')
+            if presigned_url:
+                response.append({
+                    'bucket': bucket,
+                    'folder': folder,
+                    'file_name': file_name,
+                    'presigned_url': presigned_url
+                })
+            else:
+                response.append({
+                    'bucket': bucket,
+                    'folder': folder,
+                    'file_name': file_name,
+                    'error': error
+                })
 
-        try:
-            # Check if the folder exists and create it if it doesn't
-            path = f"{folder}/{file_name}"
-            check_and_create_path(s3, bucket_name, path)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response)
+        }
 
-            # Generate a pre-signed URL for object upload
-            presigned_url = s3.generate_presigned_url(
-                'put_object',
-                Params={'Bucket': bucket_name, 'Key': path},
-                ExpiresIn=expires_in  # URL expiration time in seconds
-            )
-            presigned_urls.append({
-                "file": f"s3://{bucket_name}/{folder}/{file_name}",
-                "presigned_url": presigned_url
-            })
-        except boto3.exceptions.botocore.exceptions.ClientError as e:
-            error_code = e.response['Error']['Code']
-            error_message = e.response['Error']['Message']
-            print(f"Error Code: {error_code}, Error Message: {error_message}")
-        except Exception as e:
-            # In case of any error we append None to the list for that file
-            print(f"Error generating pre-signed URL for {folder}/{file_name}: {e}")
-            presigned_urls.append({
-                "file": f"s3://{bucket_name}/{folder}/{file_name}",
-                "presigned_url": None
-            })
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'presigned_urls': presigned_urls})
-    }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
